2f28ee64ca96831895860d12b91143fd50bbb75c
SaraKhandaker <kttsara@gmail.com>
2020-07-13 15:49:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+    let min=array[0]
+    let index=0
+    for (let i=0; i< array.length;i++){
+        if (array[i]<min){
+            min=array[i]
+            index=i
+        }
+    }
+    array.splice(index, 1)
+    return min
 }
 
 function selectionSort(array){
-
+let newArray=[]
+while (array.length>0){
+    newArray.push(findMinAndRemove(array))
+}
+return newArray
 }
